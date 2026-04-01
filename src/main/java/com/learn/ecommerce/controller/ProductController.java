bfc24eb7452fcfc78867cd67282a6789ff9eb9db
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-01 21:20:29 +0530

    Created product controller and build get product bt id HTTP req.

--- src/main/java/com/learn/ecommerce/controller/ProductController.java
+++ src/main/java/com/learn/ecommerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.learn.ecommerce.model.Product;
 import com.learn.ecommerce.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,8 @@ public class ProductController {
         return ResponseEntity.ok(productService.getAllProducts());
     }
 
+    @GetMapping("{/id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
 }
